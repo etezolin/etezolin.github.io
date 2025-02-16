b3908498b37c90a22227f9a0fcce2233080add8c
etezolin <tezolin.edison@gmail.com>
2025-02-16 18:30:34 -0300

    Updates

--- src/assets/styles/colors.ts
+++
@@ -1,7 +0,0 @@
-export const primaryColor = '#C3073F';
-export const primaryDarkColor = '#1A1A1D';
-
-export const successColor = '#caf0f8';
-export const infoColor = '#caf0f8';
-export const errorColor = '#F2AF29';
-export const warningColor = '#F2AF29';

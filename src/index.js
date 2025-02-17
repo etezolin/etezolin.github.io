79f8faaaa52fae45d6a00e82d7121a693ea0e66f
etezolin <tezolin.edison@gmail.com>
2025-02-16 21:16:24 -0300

    Corrigindo configuração de rotas para GitHub Pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter history={history}>
+    <BrowserRouter basename="/etezolin.github.io" history={history}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,20 +2,15 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/home';
 import About from '../pages/about';
 import Projects from '../pages/projects';
-// import Page404 from '../pages/NotFound';
 
 export default function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" replace />} />
-
       <Route path="/home" element={<Home />} />
-
       <Route path="/about" element={<About />} />
-
       <Route path="/projects" element={<Projects />} />
-
-      {/* <Route path="*" element={<Page404 url="/" />} /> */}
+      <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   );
 }

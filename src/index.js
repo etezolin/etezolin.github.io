7bb44c68f85fcbb9fa85285e0a7562941f78a08c
etezolin <tezolin.edison@gmail.com>
2025-02-16 21:22:56 -0300

    Build atualizado com correções das rotas

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/etezolin.github.io" history={history}>
+    <BrowserRouter history={history}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
